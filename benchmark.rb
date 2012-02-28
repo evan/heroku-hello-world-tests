acd4a626c2be9d3970a31006db7fec197d710d30
Evan Weaver <evan@cloudbur.st>
2012-02-27 23:44:37 -0800

    Cleanup.

--- benchmark.rb
+++ benchmark.rb
@@ -2,13 +2,14 @@
 login = "-i ~/.ssh/fauna-default-keypair.pem ec2-user@ec2-23-20-42-109.compute-1.amazonaws.com"
 
 servers = [
-["scala-finagle", "/"],
-["c-c", "/"],
-#["java-jetty", "/"],
-#[ "js-node", "/"],
-#["python-bottle", "/"],
-#["ruby-sinatr", "/"],
-["java-tomcat", "/servlet"]
+  # App folder and subdomain, URL
+  ["scala-finagle", "/"],
+  ["c-c", "/"],
+  ["java-jetty", "/"],
+  [ "js-node", "/"],
+  ["python-bottle", "/"],
+  ["ruby-sinatr", "/"],
+  ["java-tomcat", "/servlet"]
 ]
 
 low = 100
@@ -38,11 +39,11 @@ end
 
 servers.each do |prefix, uri|
  results = prefix + uri[1..-1] + ".csv"
- httperf = prefix + uri[1..-1] + ".out"
- command = "sudo env PATH=/usr/local/bin:$PATH autobench --single_host --host1 #{prefix}.herokuapp.com --uri1 #{uri} --low_rate #{low} --high_rate #{high} --rate_step #{step} --num_call #{calls} --const_test_time #{test_time} --timeout 30 --output_fmt csv --file #{results} 2>&1 | tee -a #{httperf}"
+ output = prefix + uri[1..-1] + ".out"
+ command = "sudo env PATH=/usr/local/bin:$PATH autobench --single_host --host1 #{prefix}.herokuapp.com --uri1 #{uri} --low_rate #{low} --high_rate #{high} --rate_step #{step} --num_call #{calls} --const_test_time #{test_time} --timeout 30 --output_fmt csv --file #{results} 2>&1 | tee -a #{output}"
 
   puts "*** #{prefix} ***"
-  File.unlink(httperf) if File.exist?(httperf)
+  File.unlink(output) if File.exist?(output)
   run("cd #{prefix} && heroku restart && heroku ps:scale web=5")
   sleep(1)
   run("ssh #{login} #{command}")
@@ -50,6 +51,6 @@ servers.each do |prefix, uri|
   run("scp #{login}:#{results} .")
 end
 
-watcher(thread)
+Thread.kill(watcher)
 
 
