7f11d5db71b71d4abf53de667c602463fd374c39
Evan Weaver <evan@cloudbur.st>
2012-02-28 20:45:31 -0800

    Some stuff.

--- chart.rb
+++ chart.rb
@@ -5,10 +5,12 @@ data = {}
 range_responses = 16250
 range_errors = 100
 range_response_time = 10000
-smoothness=0.2
+smoothness=0.75
 
 folder = ARGV[0] || "."
-dynos = ARGV[1] || "?"
+dynos = ARGV[1].to_i
+
+title = "HTTP hello-world on Heroku with #{dynos} Dyno#{'s' if dynos > 1}"
 
 Dir["#{folder}/*.csv"].each do |file|
   CSV.foreach(file) do |line|
@@ -31,12 +33,16 @@ end
 
 File.open('tmp/chart.r', 'w') do |file|
   file.puts <<-preamble
-    range_responses = #{range_responses}
-    range_response_time = #{range_response_time}
-    range_errors = #{range_errors}
-    smoothness = #{smoothness}
     data = read.table('tmp/chart.csv', header=T, sep=',')
     attach(data)
+    dynos = #{dynos}
+    dem_req_rate = dem_req_rate/dynos
+
+    range_response_time = #{range_response_time}
+    range_errors = #{range_errors}
+    range_responses = max(dem_req_rate)
+    smoothness = #{smoothness}
+
     quartz('Throughput', 8, 6)
     par(mar=c(5,5,5,5))
     xl <- seq(min(dem_req_rate),max(dem_req_rate), (max(dem_req_rate) - min(dem_req_rate))/1000)
@@ -63,6 +69,7 @@ File.open('tmp/chart.r', 'w') do |file|
   data["dem_req_rate"].each do |y|
     next unless y =~ /avg_rep_rate_/
     file.puts <<-plotter
+      #{y} = #{y}/dynos
       par(new=T)
       plot(dem_req_rate, #{y}, ylim=c(0,range_responses), axes=F, ann=F, type='n')
       smooth = smooth.spline(dem_req_rate, #{y}, spar=smoothness)
@@ -87,16 +94,16 @@ File.open('tmp/chart.r', 'w') do |file|
     par(new=T)
     plot(dem_req_rate, dem_req_rate, ylim=c(0,range_responses), axes=F, ann=F, type='n')
     abline(0,1, lty=3, col='gray')
-    title(main='Throughput of HTTP hello-world on Heroku with #{dynos} Dynos')
+    title(main='Throughput of #{title}')
     axis(1)
     axis(2)
-    legend(range_responses/6.5, range_responses, c(#{labels.inspect[1..-2]}), cex=0.8, col=c(#{colors.inspect[1..-2]}), pch=21:22)
+    legend(range_responses/6.5, range_responses, c(#{labels.inspect[1..-2]}), cex=0.8, col=c(#{colors.inspect[1..-2]}), pch=21:22, bg='white')
 
     par(new=T)
     plot(dem_req_rate, dem_req_rate, ylim=c(0,range_errors), axes=F, ann=F, type='n')
     axis(4)
 
-    title(xlab="Requests per second", ylab="Responses per second")
+    title(xlab="Requests per second per dyno", ylab="Responses per second per dyno")
     title(ylab="% connection errors", line=-34)
 
     box()
@@ -121,11 +128,11 @@ File.open('tmp/chart.r', 'w') do |file|
 
   file.puts <<-postamble
     par(new=T)
-    title(main='Latency of HTTP hello-world on Heroku with #{dynos} Dynos')
+    title(main='Latency of #{title}')
     axis(1)
     axis(2, at=axTicks(2), c(0, 10, 100, 1000, 10000))
-    legend(range_responses*0.76, log10(range_response_time)*0.35, c(#{labels.inspect[1..-2]}), cex=0.8, col=c(#{colors.inspect[1..-2]}), pch=21:22)
-    title(xlab="Requests per second", ylab="Milliseconds (log scale)")
+    legend(range_responses*0.83, log10(range_response_time)*0.35, c(#{labels.inspect[1..-2]}), cex=0.8, col=c(#{colors.inspect[1..-2]}), pch=21:22, bg='white')
+    title(xlab="Requests per second per dyno", ylab="Milliseconds (log scale)")
 
     box()
     Sys.sleep(30)
